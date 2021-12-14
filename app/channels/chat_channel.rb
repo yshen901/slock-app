26ec60f2cc2be53fde718573168095a2e0131493
Yuci Shen <shen.yuci1@gmail.com>
2021-12-14 03:52:34 -0800

    Add field to message to let the app know if the recipient needs to activate a dm_channel.

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -6,20 +6,29 @@ class ChatChannel < ApplicationCable::Channel
 
   def speak(data)
     message_data = data['message']
-    message = Message.create(
+    message = Message.new(
       body: message_data['body'], 
       user_id: message_data['user_id'], 
       channel_id: message_data['channel_id']
     )
-    socket = { 
-      message: {
-        body: message_data['body'],
-        created_at: message_data['created_at'],
-        user_id: message_data['user_id'],
-        channel_id: message_data['channel_id']
+
+    if message.save 
+      channel = message.channel
+
+      # If the message's channel is a dm_channel, send up activate_dm_channel to tell the app to activate it
+      activate_dm_channel = channel.dm_channel && channel.active_1 && channel.active_2
+
+      socket = { 
+        message: {
+          body: message_data['body'],
+          created_at: message_data['created_at'],
+          user_id: message_data['user_id'],
+          channel_id: message_data['channel_id'],
+          activate_dm_channel: activate_dm_channel
+        }
       }
-    }
-    ChatChannel.broadcast_to('chat_channel', socket)
+      ChatChannel.broadcast_to('chat_channel', socket)
+    end
   end
 
   def unsubscribed
