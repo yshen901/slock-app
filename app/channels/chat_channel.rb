b6aaefecfbbbebf0fee7d6bc416b06d22b42b78a
Yuci Shen <shen.yuci1@gmail.com>
2022-01-11 00:49:21 -0800

    Add condition in chatChannel where messages are only broadcasted if the creation was a success, using new+save instead of create to determine success.

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -9,43 +9,46 @@ class ChatChannel < ApplicationCable::Channel
 
     # handles new message post request
     if message_data['type'] == "PUT"
-      message = Message.create(
+      message = Message.new(
         body: message_data['body'], 
         user_id: message_data['user_id'], 
         channel_id: message_data['channel_id']
       )
-
-      # If the message's channel is a dm_channel and one user is inactive
-      channel = message.channel
-      connection = channel.dm_channel_connection
-      if connection
-        activate_dm_channel = !(connection.active_1 && connection.active_2)
-      end
-
-      # Activate the link behind the scenes
-      if activate_dm_channel 
-        connection.update(
-          active_1: true,
-          active_2: true
-        )
-      end
-
-      # Passes up activate_dm_channel to tell the app to fire startDmChannel, which loads the channel
-      socket = { 
-        message: {
-          type: "PUT",
-          body: message_data['body'],
-          created_at: message_data['created_at'],
-          user_id: message_data['user_id'],
-          channel_id: message_data['channel_id'],
-          activate_dm_channel: activate_dm_channel,
-          id: message.id,
-          total_reacts: {},
-          user_reacts: {}
+      
+      # use new+save instead of create so we can determine whether it failed or succeeded
+      # only broadcast if it worked
+      if message.save 
+        # If the message's channel is a dm_channel and one user is inactive
+        channel = message.channel
+        connection = channel.dm_channel_connection
+        if connection
+          activate_dm_channel = !(connection.active_1 && connection.active_2)
+        end
+  
+        # Activate the link behind the scenes
+        if activate_dm_channel 
+          connection.update(
+            active_1: true,
+            active_2: true
+          )
+        end
+  
+        # Passes up activate_dm_channel to tell the app to fire startDmChannel, which loads the channel
+        socket = { 
+          message: {
+            type: "PUT",
+            body: message_data['body'],
+            created_at: message_data['created_at'],
+            user_id: message_data['user_id'],
+            channel_id: message_data['channel_id'],
+            activate_dm_channel: activate_dm_channel,
+            id: message.id,
+            total_reacts: {},
+            user_reacts: {}
+          }
         }
-      }
-      ChatChannel.broadcast_to('chat_channel', socket)
-
+        ChatChannel.broadcast_to('chat_channel', socket)
+      end
     # handles delete message request
     elsif message_data['type'] == "DELETE"
       ChatChannel.broadcast_to('chat_channel', {
